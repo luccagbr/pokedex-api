1e78a3ed746fd55faea30203ca9a0f1e942f5d93
Lucca <luccavitalc@gmail.com>
2025-10-13 15:45:25 -0300

    feat: Realizada a mudança do middleware de cors e adicionada a biblioteca de jsonwebtoken

--- src/middlewares/corsMiddleware.ts
+++ src/middlewares/corsMiddleware.ts
@@ -1,42 +1,25 @@
 import { Request } from "express";
 
 export function corsMiddleware(req: Request, callback: CallableFunction) {
-    const envStatus: "development" | "production" | string = process.env.NODE_ENV;
-
-    if (
-        !(process.env.NODE_ENV === "development" && process.env.FRONTEND_URL_LOCAL) ||
-        !(process.env.FRONTEND_URL_PRODUCTION === "production" && process.env.FRONTEND_URL_PRODUCTION)
-    ) {
-        throw new Error("Nenhuma URL informada!");
-    }
-
     let urlActive = undefined;
-
-    switch (envStatus) {
-        case "development":
-            urlActive = process.env.FRONTEND_URL_LOCAL;
-            break;
-        case "production":
-            urlActive = process.env.FRONTEND_URL_PRODUCTION;
-            break;
-    }
 
     let corsOptions: any = {
         origin: urlActive,
         optionsSuccessStatus: 204,
-        credentials: true,
-        allowedHeaders: ["Content-Type", "Authorization"],
     };
 
     if (req.path === "/api/auth/login") {
         corsOptions = {
             ...corsOptions,
             methods: ["OPTIONS", "POST", "PUT"],
+            credentials: true,
+            origin: process.env.FRONTEND_URL,
         };
     } else {
         corsOptions = {
             ...corsOptions,
             methods: ["POST", "GET", "PUT", "DELETE", "OPTIONS"],
+            allowedHeaders: ["Content-Type", "Authorization"],
         };
     }
 
